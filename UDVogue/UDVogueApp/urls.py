32ee7b99855ac49570affa932882e28187ae177b
izarofserrano <i.serrano@opendeusto.es>
2024-11-04 16:35:52 +0100

    Incluir vistas

--- UDVogue/UDVogueApp/urls.py
+++ UDVogue/UDVogueApp/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='listaE'),
+    path('listadoDeEditoriales/', views.listaEditoriales, name='listaE'),
+    path('editoriales/<int:id_editorial>/', views.detalleEDITORIAL, name="detalle"),
 ]
--- UDVogue/UDVogueApp/views.py
+++ UDVogue/UDVogueApp/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render
 from .models import Editorial
+from django.http import HttpResponse, HttpResponseNotFound
+
 
 # Create your views here.
 
+def index(request):
+    return HttpResponse('primera vista')
+
 def listaEditoriales(request):
     editoriales = Editorial.objects.order_by('nombre')
-    nombre_editoriales = ', '.join([editoriales.nombre for editorial in editoriales])
+    nombre_editoriales = ', '.join([editorial.nombre for editorial in editoriales])
     return HttpResponse(nombre_editoriales)
+
+def detalleEDITORIAL(request, id_editorial):
+    try:
+        editorial = Editorial.objects.get(pk=id_editorial) 
+        revistas = editorial.revistas.all()
+
+        cadenaTxt = f"{editorial.nombre} - Contacto: {editorial.contacto}"
+
+        if revistas.exists():
+            cadenaTxt += "Revistas:\n"
+            for revista in revistas:
+                cadenaTxt += f"- {revista.titulo}, Fecha Publicación: {revista.fecha_publi}, Nº Edición: {revista.numero_edicion}"
+            else:
+                cadenaTxt += "No hay revistas asociados a esta editorial."
+            return HttpResponse(cadenaTxt)
+        except Editorial.DoesNotExist:
+            return HttpResponseNotFound("Editorial no econtrada")
